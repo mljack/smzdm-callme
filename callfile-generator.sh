d74adfa77d1e75fe2f90f23f4c7518c5c566eb66
magang <magang@gmail.com>
2013-06-04 08:26:58 +0000

    dahdi

--- callfile-generator.sh
+++ callfile-generator.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #Change it to YOUR SIP TRUNK
+#此处是你Asterisk外呼Trunk的信息
+#SIP或者DAHDI
 SIPCHANNEL="SIP/8610xxxxxxx/YOURMOBILEPHONE"
 TITLE=$(echo $1 | sed 's/ *$//g')
 sudo tee "/tmp/smzdm$RANDOM.call" > /dev/null <<EOF
