bea8cd3fdf898859f1886f599f7465267b3aef14
magang <magang@gmail.com>
2013-06-05 01:12:10 +0000

    change /etc/rc.local

--- mv_call_file_to_asterisk_outgoing_folder.sh
+++ mv_call_file_to_asterisk_outgoing_folder.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Monitor new callfile in /tmp, 
 # if there is any, move it to Asterisk outgoing folder, so a call will be initialized
+# Add a line to /etc/rc.local
+# So this script will run after bootup
+# nohup /root/mv_call_file_to_asterisk_outgoing_folder.sh 0<&- &>/dev/null &
+
 while [ "true" ]
 do
   mv /tmp/*.call /var/spool/asterisk/outgoing/ >/dev/null 2>&1
