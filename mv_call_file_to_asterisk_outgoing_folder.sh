628454975af9b2ca725a94bb9a052d0611125a3d
magang <magang@gmail.com>
2013-06-05 01:10:50 +0000

    adding description

--- mv_call_file_to_asterisk_outgoing_folder.sh
+++ mv_call_file_to_asterisk_outgoing_folder.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Monitor new callfile in /tmp, 
+# if there is any, move it to Asterisk outgoing folder, so a call will be initialized
 while [ "true" ]
 do
   mv /tmp/*.call /var/spool/asterisk/outgoing/ >/dev/null 2>&1
