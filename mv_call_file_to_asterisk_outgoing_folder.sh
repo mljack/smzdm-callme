5c91e0067dde0ebfb6100a1ae3768a4a867ba944
magang@gmail.com <magang@gmail.com@2fd0bcd2-79b6-0069-c50d-3fa96d766444>
2013-06-06 02:47:16 +0000

    A 'better practice' is to create the call file in a temporary directory on the same device, write to it, close it and 'mv' it. 'mv' is an 'atomic' operation.

--- mv_call_file_to_asterisk_outgoing_folder.sh
+++ mv_call_file_to_asterisk_outgoing_folder.sh
@@ -5,6 +5,10 @@
 # So this script will run after bootup
 # nohup /root/mv_call_file_to_asterisk_outgoing_folder.sh 0<&- &>/dev/null &
 
+
+# A 'better practice' is to create the call file in a temporary directory on the same 
+# device, write to it, close it and 'mv' it. 'mv' is an 'atomic' operation.
+
 while [ "true" ]
 do
   mv /tmp/*.call /var/spool/asterisk/outgoing/ >/dev/null 2>&1
